aaf5e314a9cfba4784300c86b16e697fb89bbd13
joel santos <joelsantosjunior@outlook.com>
2024-05-26 20:06:21 -0300

    adjust watermark position and size

--- src/services/videoService.mjs
+++ src/services/videoService.mjs
@@ -1,17 +1,13 @@
 import Ffmpeg from "fluent-ffmpeg"
-import { createReadStream, createWriteStream } from "fs"
-import path from "path"
+
+// Calculate the overlay position (centered at the bottom)
+const overlayPosition = "x=(main_w-overlay_w)/2:y=main_h-(overlay_h * 2)"
 
 export const addWatermark = async (videoPath, watermarkPath, outputPath) => {
   return new Promise((resolve, reject) => {
     Ffmpeg(videoPath)
       .input(watermarkPath)
-      .complexFilter([
-        {
-          filter: "overlay",
-          options: { x: 10, y: 10 },
-        },
-      ])
+      .complexFilter([`[0][1]overlay=${overlayPosition}`])
       .on("end", () => {
         console.log("Watermark added to video")
         resolve(outputPath)
